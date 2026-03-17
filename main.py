dfabe607be9b4e2df85faee3b36a0c49bf85c82d
Guru9115 <meguru.ra@gmail.com>
2026-03-17 21:16:47 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="NAC Loadmaster", layout="wide")
+
+st.title("✈️ NAC A320 LOADMASTER SYSTEM")
+
+st.markdown("""
+### Modules:
+- Loadsheet
+- LDM Parser
+- Dashboard
+
+👉 Use sidebar to navigate
+""")
